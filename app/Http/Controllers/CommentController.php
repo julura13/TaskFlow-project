40e7f3ed01945c9957a997602c442a3fdc9706d0
julura13 <juan@julura.co.za>
2026-02-22 18:08:13 +0200

    Updates to comment workflow
    Added Comment Policy and comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentStoreForTaskRequest;
 use App\Http\Requests\CommentStoreRequest;
 use App\Http\Requests\CommentUpdateRequest;
 use App\Models\Comment;
+use App\Models\Task;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class CommentController extends Controller
 {
+    public function storeForTask(CommentStoreForTaskRequest $request, Task $task): RedirectResponse
+    {
+        $task->comments()->create([
+            'body' => $request->validated('body'),
+            'user_id' => $request->user()->id,
+        ]);
+
+        return redirect()->route('tasks.show', $task)->with('status', __('Comment added.'));
+    }
+
     public function index(Request $request): View
     {
         $comments = Comment::all();
@@ -27,11 +39,11 @@ class CommentController extends Controller
 
     public function store(CommentStoreRequest $request): RedirectResponse
     {
-        $comment = Comment::create($request->validated());
+        $data = $request->validated();
+        $data['user_id'] = $request->user()->id;
+        $comment = Comment::create($data);
 
-        $request->session()->flash('comment.id', $comment->id);
-
-        return redirect()->route('comments.index');
+        return redirect()->route('tasks.show', $data['task_id'])->with('status', __('Comment added.'));
     }
 
     public function show(Request $request, Comment $comment): View
