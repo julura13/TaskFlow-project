c7a3146fbdc8fe07e19e209483f6016010d07fa9
julura13 <juan@julura.co.za>
2026-02-24 07:13:49 +0200

    Make changes to the routes and do cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProfileController;
-use App\Models\Project;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('/dashboard', function () {
-    $projects = Project::latest()->get();
-    return view('dashboard', ['projects' => $projects]);
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', [PagesController::class, 'dashboard'])->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('projects', App\Http\Controllers\ProjectController::class);
+    Route::patch('tasks/{task}/status', [App\Http\Controllers\TaskController::class, 'updateStatus'])->name('tasks.update-status');
+    Route::patch('tasks/{task}/assignee', [App\Http\Controllers\TaskController::class, 'updateAssignee'])->name('tasks.update-assignee');
     Route::resource('tasks', App\Http\Controllers\TaskController::class);
     Route::post('tasks/{task}/comments', [App\Http\Controllers\CommentController::class, 'storeForTask'])->name('tasks.comments.store');
+    Route::delete('tasks/{task}/comments/{comment}', [App\Http\Controllers\CommentController::class, 'destroyFromTask'])->name('tasks.comments.destroy');
 });
 
 require __DIR__.'/auth.php';
